38632446da5dfe6b3ea5263e6582af02401e44eb
Maya <salkiewicz.maya@gmail.com>
2023-02-01 12:07:29 +0100

    creating hamburger menu

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ window.addEventListener("scroll", () => {
     navigation.classList.remove("sticky");
   }
 });
+
+const menuBtn = document.querySelector(".hamburger");
+const menuList = document.querySelector(".mobile-nav");
+
+menuBtn.addEventListener("click", function () {
+  menuBtn.classList.toggle("is-active");
+  menuList.classList.toggle("is-active");
+});
+
+menuList.addEventListener("click", function () {
+  menuList.classList.remove("is-active");
+});
